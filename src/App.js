03cfc0b39d283984dcbe0d84faeec022fedf1d5d
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-11-06 14:36:22 +0800

    display list of names by using useList and useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NameTag from "./components/name-tag";
+import Age from "./components/age";
+import useList from "./components/hooks/useList";
+
+import "./App.scss";
+
+const initialNames = [
+  {
+    firstName: "john",
+    lastName: "johnson"
+  },
+  {
+    firstName: "peter",
+    lastName: "peterson"
+  },
+  {
+    firstName: "abyss",
+    lastName: "abyssson"
+  }
+];
 
 function App() {
+  const items = useList(initialNames);
+  const [editable, setEditable] = useState(false);
+
+  function removeHandler(e) {
+    items.removeItem(e.target.name);
+  }
+
+  function doubleClickHandler() {
+    setEditable(true);
+  }
+
+  function onKeyPress(e, index) {
+    if (e.keyCode === 13 || e.charCode === 13) {
+      setEditable(false);
+      items.updateItem(index, e.target.value);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column"
+          }}
         >
-          Learn React
-        </a>
+          {items.list.map((v, i) => {
+            return (
+              <NameTag
+                key={i}
+                firstName={v.firstName}
+                lastName={v.lastName}
+                onClick={removeHandler}
+                editable={editable}
+                name={v.firstName}
+                onDoubleClick={doubleClickHandler}
+                onKeyPress={onKeyPress}
+                index={i}
+              />
+            );
+          })}
+        </div>
+        <br />
+        <Age />
       </header>
+      <hr />
     </div>
   );
 }
