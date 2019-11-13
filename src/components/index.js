68b31fb0338c605378e2caa0d8169551e1d44a35
RakeshCheekatimala <rakeshcheekatimala@gmail.com>
2019-11-13 12:52:56 +0800

    extracting all the components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Content } from "./content";
+export { default as Input } from "./input";
+export { default as ListExample } from "./list-example";
+export { default as Nametag } from "./name-tag";
+export { default as Navbar } from "./navbar";
+export { default as GitHubProfile } from "./github-profile";
+export { default as Age } from "./age";
+export { default as UserBackground } from "./userBackground";
